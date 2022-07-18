4ddc9abc93b1c2074118adb6e8504b4101d96f59
Fabian Vallejos <vallejosfab@gmail.com>
2022-07-17 20:29:25 -0400

    Updated API endpoint url

--- src/components/pages/PageProducts/services/products.ts
+++ src/components/pages/PageProducts/services/products.ts
@@ -1,5 +1,5 @@
 export const getProductsList = async () => {
-  const apiUrl = `https://p7jb1wjlz4.execute-api.us-east-1.amazonaws.com/dev/products`;
+  const apiUrl = `https://cb5fun3bo4.execute-api.us-east-1.amazonaws.com/dev/products`;
   const response = await fetch(apiUrl);
   const data = await response.json();
   return data;
