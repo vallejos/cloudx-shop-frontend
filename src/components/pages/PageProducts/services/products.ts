a3d426f76e8d1ed36e753ace180afe7646567445
Fabian Vallejos <vallejosfab@gmail.com>
2022-08-07 18:56:59 -0400

    Updated products API

--- src/components/pages/PageProducts/services/products.ts
+++ src/components/pages/PageProducts/services/products.ts
@@ -1,7 +1,7 @@
 import { Product } from "models/Product";
 
 export const getProductsList = async () => {
-  const apiUrl = `https://cb5fun3bo4.execute-api.us-east-1.amazonaws.com/dev/products`;
+  const apiUrl = `https://tdxvfwtpad.execute-api.us-east-1.amazonaws.com/dev/products`;
   const response = await fetch(apiUrl);
   const data = await response.json();
   return data.products as Product[];
